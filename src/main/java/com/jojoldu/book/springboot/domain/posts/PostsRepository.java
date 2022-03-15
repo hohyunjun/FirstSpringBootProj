b7c1de1446e501c40d38db829420172f5ae43307
hohyunjun <hohyunjun@lgcns.com>
2022-03-15 22:35:38 +0900

    Added basic screen and save, delete, update, get service

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // Mybatis 등에서 Dao 로 불리는 DB Layer 접근자.
 // JPA 에선 Repository 라고 부르며 인터페이스로 생성함.
@@ -10,4 +13,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // 일반적으로 두개 파일은 domain 패키지에서 함께 관리함.
 public interface PostsRepository extends JpaRepository<Posts, Long>{
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
